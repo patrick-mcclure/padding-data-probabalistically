aa801bbe992b5f3dfe34baf07c7e67658abe13ca
Patrick McClure <patrickmcclure@mac00070.ern.nps.edu>
2022-03-24 15:27:26 -0700

    Constrained new zs to be within six of zero

--- prob-data-pad.py
+++ prob-data-pad.py
@@ -6,8 +6,8 @@ import math
 import pickle
 
 n_epochs = 200
-learning_rate = 0.1
-n_examples = 1000
+learning_rate = 1.0
+
 n_components_per_epoch = 5000
 data_folder_path = ''
 
@@ -17,14 +17,12 @@ with open('codings/mu.pkl','rb') as f:
     mus = pickle.load(f)
 with open('codings/sigma.pkl','rb') as f:
     sigmas = pickle.load(f)
-rand_inds = list(range(mus.shape[0]))                           
+rand_inds = list(range(mus.shape[0])) 
+shuffle(rand_inds)
 maxes = mus.max(axis=0)
 mins = mus.min(axis=0)
-zs = np.zeros([n_examples,mus.shape[1]])
-for i in range(mus.shape[1]):
-    zs[:,i] = np.random.uniform(low = mins[i], high = maxes[i], size = n_examples) 
-#zs = (mus + mus[rand_inds,:])/2.0
-
+zs = np.concatenate([mus,(mus + mus[rand_inds,:])/2.0],axis=0)
+n_examples = zs.shape[0]
 zs = torch.from_numpy(zs)
 zs.requires_grad=True
 print(mus.shape)
@@ -43,30 +41,13 @@ for e in range(n_epochs):
     max_components_log_prob_sum = 0.0
     for i in range(n_examples):
         log_prob = -torch.square(mus - zs[i])/(2.0*sigmas_squared) - 0.5*math.log(2.0*math.pi) - torch.log(sigmas) 
-        #prob = torch.exp(-torch.square(mus - zs[i])/(2.0*sigmas_squared))/(math.sqrt(2.0*math.pi)*sigmas)
-        #print("LOG_PROB")
-        #print(log_prob)
         log_prob_sum = torch.sum(log_prob,dim=1)
-        #prob_prod = torch.prod(prob,dim=1)
-        #print("LOG_PROB_SUM")
-        #print(log_prob_sum)
-        max_pool = torch.nn.MaxPool1d(mus.shape[0])
-        max_component_log_prob = max_pool(torch.reshape(log_prob_sum,[1,1,-1]))
-        #print("MAX_COMPONENT")
-        #print(max_component_log_prob)
+        max_component_log_prob = torch.max(log_prob_sum)
         max_components_log_prob_sum += torch.sum(max_component_log_prob)/n_examples
-        #max_components_log_prob_sum += torch.sum(prob_prod)/(n_examples*mus.shape[0])
     print("MAX_COMPONENT_SUM")
     print(max_components_log_prob_sum)
     optimizer.zero_grad()
     max_components_log_prob_sum.backward()
     optimizer.step()
-    #scheduler.step()
     zs_np = zs.detach().numpy()
-    #for i in range(mus.shape[1]):
-        #zs_np[:,i] = np.clip(zs_np[:,i],mins[i],maxes[i])
-    #print(zs_np)
-
-    
-np.save('new_zs.npy',zs_np)
- 
+    np.save('new_zs/new_zs_'+str(e)+'.npy',np.clip(zs_np,a_min=-6.0,a_max=6.0))
